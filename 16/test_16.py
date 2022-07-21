cfc3c8641692c3b611b15ed1ed40cabd433e8647
Michael Spitznagel <exultavit@gmail.com>
2022-07-21 15:29:18 -0400

    Create placeholder for day 16

---
+++ 16/test_16.py
@@ -0,0 +1,20 @@
+"""Tests for day 16 of Advent of Code 2021."""
+
+import day16
+
+# Test data given as a multiline string.
+sample_input_data = """Sample data not entered"""
+
+sample_solution_a = "Sample solution not entered"
+
+sample_solution_b = "Sample solution not entered"
+
+
+def test_part_a():
+    """Test the solution on sample data for part A."""
+    assert day16.part_a(sample_input_data) == sample_solution_a
+
+
+def test_part_b():
+    """Test the solution on sample data for part B."""
+    assert day16.part_b(sample_input_data) == sample_solution_b
