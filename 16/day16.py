98107ca27fbfd6b39431e266b6f6665d099a3bb3
Michael Spitznagel <exultavit@gmail.com>
2022-07-22 11:47:00 -0400

    Solve day 16

--- 16/day16.py
+++ 16/day16.py
@@ -1,18 +1,131 @@
 """Solves day 16, Advent of Code 2021."""
 
 from aocd.models import Puzzle
+from math import prod
+
+def parse_input(input_data: str) -> tuple:
+    """Given a hex string, return a tuple of bits."""
+    hex_to_nibbles = {digit: f"{int(digit, 16):04b}"
+                   for digit in "0123456789ABCDEF"}
+    nibbles = [hex_to_nibbles[ch] for ch in input_data]
+    bits = tuple(int(bit)
+                 for x in nibbles
+                 for bit in x)
+    return bits
+
+
+def parse_binary(bits: tuple) -> int:
+    """Convert a tuple of bits to an integer value."""
+    
+    n = 0
+    for bit in bits:
+        n *= 2
+        n += bit
+    return n
+
+
+class Packet:
+    """Interprets a tuple of bits as a packet."""
+    
+    def __init__(self, data: tuple):
+        """Initializes the packet with a tuple of bits.
+
+        All subpackets are created and initialized as well.
+        """
+        self.data = data
+        self.subpackets = []
+        self.value, self.length = self.find_value_and_length()
+
+    def find_value_and_length(self) -> (int, int):
+        """Find this packet's value and total length, depending on type ID."""
+
+        if self.type_ID == 4:
+            return self.__literal_value_and_length()
+        else:
+            return self.__operator_value_and_length()
+
+    @property
+    def version(self) -> int:
+        """Packet version number"""
+        return parse_binary(self.data[0:3])
+
+    @property
+    def type_ID(self) -> int:
+        """Packet type ID"""
+        return parse_binary(self.data[3:6])
+    
+    def __literal_value_and_length(self) -> (int, int):
+        """Return value and length for literal value type packet."""
+
+        group_index = 6
+        value = 0
+        while True:
+            value <<= 4
+            value += parse_binary(self.data[group_index+1:group_index+5])
+            if self.data[group_index] == 0:
+                return value, group_index + 5
+            group_index += 5
+
+    def __operator_value_and_length(self) -> (int, int):
+        """Return value and length for operator type packet."""
+
+        def append_next_subpacket(idx):
+            """Read and append subpacket at idx."""
+            p = Packet(self.data[idx:])
+            self.subpackets.append(p)
+
+        length = None
+        length_type_ID = self.data[6]
+        if length_type_ID == 0:
+            idx = 22
+            total_subpacket_length = parse_binary(self.data[7:22])
+            length = idx + total_subpacket_length
+            while idx < length:
+                append_next_subpacket(idx)
+                idx += self.subpackets[-1].length
+                
+        elif length_type_ID == 1:
+            idx = 18
+            total_subpacket_number = parse_binary(self.data[7:18])
+            for j in range(total_subpacket_number):
+                append_next_subpacket(idx)
+                idx += self.subpackets[-1].length
+            length = idx
+
+        value = self.__evaluate_operator()
+        return value, length
+
+    def __evaluate_operator(self) -> int:
+        """Evaluate operator packet applied to subpacket values."""
+        
+        id = self.type_ID
+        sp_values = [sp.value for sp in self.subpackets]
+        if id == 0: return sum(sp_values)
+        if id == 1: return prod(sp_values)
+        if id == 2: return min(sp_values)
+        if id == 3: return max(sp_values)
+        if id == 5: return int(sp_values[0] > sp_values[1])
+        if id == 6: return int(sp_values[0] < sp_values[1])
+        if id == 7: return int(sp_values[0] == sp_values[1])
+            
+
+def packet_version_sum(p: Packet):
+    """Sums the version numbers of the packet and subpackets."""
+    return p.version + sum([packet_version_sum(sp) for sp in p.subpackets])
 
 
 def part_a(input_data: str) -> int:
     """Given the puzzle input data, return the solution for part A."""
 
-    return "Solution not implemented"
+    p = Packet(parse_input(input_data))
+    return packet_version_sum(p)
 
 
 def part_b(input_data: str) -> int:
     """Given the puzzle input data, return the solution for part B."""
 
-    return "Solution not implemented"
+    p = Packet(parse_input(input_data))
+    return p.value
 
 
 if __name__ == '__main__':
--- 16/test_16.py
+++ 16/test_16.py
@@ -2,19 +2,81 @@
 
 import day16
 
-# Test data given as a multiline string.
-sample_input_data = """Sample data not entered"""
+def test_version_sums():
+    """Test packet version sums."""
 
-sample_solution_a = "Sample solution not entered"
+    sample_inputs = ["8A004A801A8002F478", "620080001611562C8802118E34", "C0015000016115A2E0802F182340", "A0016C880162017C3686B18A3D4780"]
 
-sample_solution_b = "Sample solution not entered"
+    sample_version_sums = [16, 12, 23, 31]
 
-
-def test_part_a():
-    """Test the solution on sample data for part A."""
-    assert day16.part_a(sample_input_data) == sample_solution_a
+    for test_input, test_sum in zip(sample_inputs, sample_version_sums):
+        assert day16.part_a(test_input) == test_sum
 
 
-def test_part_b():
-    """Test the solution on sample data for part B."""
-    assert day16.part_b(sample_input_data) == sample_solution_b
+def test_sum_operation():
+    """Test: C200B40A82 finds the sum of 1 and 2, resulting in the value 3."""
+
+    p = day16.Packet(day16.parse_input('C200B40A82'))
+    assert(len(p.subpackets) == 2)
+    assert(p.subpackets[0].value == 1)
+    assert(p.subpackets[1].value == 2)
+    assert(p.value == 3)
+
+def test_prod_operation():
+    """04005AC33890 finds the product of 6 and 9, resulting in the value 54."""
+    p = day16.Packet(day16.parse_input('04005AC33890'))
+    assert(len(p.subpackets) == 2)
+    assert(p.subpackets[0].value == 6)
+    assert(p.subpackets[1].value == 9)
+    assert(p.value == 54)
+
+def test_min_operation():
+    """880086C3E88112 finds the minimum of 7, 8, and 9, resulting in the value 7."""
+    p = day16.Packet(day16.parse_input('880086C3E88112'))
+    assert(len(p.subpackets) == 3)
+    assert(p.subpackets[0].value == 7)
+    assert(p.subpackets[1].value == 8)
+    assert(p.subpackets[2].value == 9)
+    assert(p.value == 7)
+
+def test_max_operation():
+    """CE00C43D881120 finds the maximum of 7, 8, and 9, resulting in the value 9."""
+    p = day16.Packet(day16.parse_input('CE00C43D881120'))
+    assert(len(p.subpackets) == 3)
+    assert(p.subpackets[0].value == 7)
+    assert(p.subpackets[1].value == 8)
+    assert(p.subpackets[2].value == 9)
+    assert(p.value == 9)
+
+def test_lt_operation():
+    """D8005AC2A8F0 produces 1, because 5 is less than 15."""
+    p = day16.Packet(day16.parse_input('D8005AC2A8F0'))
+    assert(len(p.subpackets) >= 2)
+    assert(p.subpackets[0].value == 5)
+    assert(p.subpackets[1].value == 15)
+    assert(p.value == 1)
+
+def test_gt_operation():
+    """F600BC2D8F produces 0, because 5 is not greater than 15."""
+    p = day16.Packet(day16.parse_input('F600BC2D8F'))
+    assert(len(p.subpackets) >= 2)
+    assert(p.subpackets[0].value == 5)
+    assert(p.subpackets[1].value == 15)
+    assert(p.value == 0)
+
+def test_eq_operation():
+    """9C005AC2F8F0 produces 0, because 5 is not equal to 15."""
+    p = day16.Packet(day16.parse_input('9C005AC2F8F0'))
+    assert(len(p.subpackets) >= 2)
+    assert(p.subpackets[0].value == 5)
+    assert(p.subpackets[1].value == 15)
+    assert(p.value == 0)
+    
+def test_compound_operation():
+    """9C0141080250320F1802104A08 produces 1, because 1 + 3 = 2 * 2."""
+    p = day16.Packet(day16.parse_input('9C0141080250320F1802104A08'))
+    assert(len(p.subpackets) == 2)
+    assert(p.subpackets[0].value == 4)
+    assert(p.subpackets[1].value == 4)
+    assert(p.value == 1)
+
