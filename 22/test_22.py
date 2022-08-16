39d5451f541db127f5591fd16bdc6c49cee94776
Michael Spitznagel <exultavit@gmail.com>
2022-08-16 10:52:21 -0400

    Create day 22 files

---
+++ 22/test_22.py
@@ -0,0 +1,20 @@
+"""Tests for day 22 of Advent of Code 2021."""
+
+import day22
+
+# Test data given as a multiline string.
+sample_input_data = """Sample data not entered"""
+
+sample_solution_a = "Sample solution not entered"
+
+sample_solution_b = "Sample solution not entered"
+
+
+def test_part_a():
+    """Test the solution on sample data for part A."""
+    assert day22.part_a(sample_input_data) == sample_solution_a
+
+
+def test_part_b():
+    """Test the solution on sample data for part B."""
+    assert day22.part_b(sample_input_data) == sample_solution_b
